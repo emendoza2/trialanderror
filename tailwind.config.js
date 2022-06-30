529456c92679b68a0428945a131be75823cc19cb
Elijah Mendoza <elijah@parentschooling.com>
2022-06-30 16:17:27 +0800

    CHange fonts, update layout and site data

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         redaction70: ["Redaction70", "Redaction", "serif"],
         tte: ["TTB", "Courier New", "Courier", "monospace"],
         tte: ["TTE", "Courier New", "Courier", "monospace"],
-        computermoderntypewriter: ['"Computer Modern Typewriter"', "Courier New", "Courier", "monospace"]
+        apfel: ['"Apfel Grotezk"', "Helvetica Neue", "Helvetica", "Arial", "sans-serif"]
       }
     },
   },
